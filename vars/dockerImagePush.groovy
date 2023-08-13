99046c3323bb24e095b1cad293a063527d766851
Vinod Pal <vinod161191@gmail.com>
2023-08-13 21:35:52 +0530

    docker image push variable

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -4,6 +4,11 @@ def call(String registryUser, String appName, String tagName){
         sh "docker login -u $registryUser -p 'DOCKERHUB_CREDENTIAL'"
     }
 
+    withCredentials([usernamePassword(credentialsId: 'docker-credential', passwordVariable: 'DOCKER_PASS', usernameVariable: 'DOCKER_USER')]) {
+        // some block
+        sh "docker login -u $DOCKER_USER -p $DOCKER_PASS"
+    }
+
     sh "docker image push ${registryUser}/${appName} ${registryUser}/${appName}:${tagName}"
     sh "docker image push ${registryUser}/${appName} ${registryUser}/${appName}:latest"
 }
