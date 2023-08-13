f9025bddf296d33240652cdce7e5362145d4bc71
Vinod Pal <vinod161191@gmail.com>
2023-08-13 16:49:54 +0530

    docker image build variable

--- vars/dockerImageBuild.groovy
+++ vars/dockerImageBuild.groovy
@@ -1,7 +1,7 @@
 def call(String registryUser, String appName, String tagName){
     sh """
         docker image build -t ${registryUser}/${appName} .
-        docker image tag -t ${registryUser}/${appName}:${tagName}
-        docker image tag -t ${registryUser}/${appName}:latest
+        docker image tag ${registryUser}/${appName}:${tagName}
+        docker image tag ${registryUser}/${appName}:latest
     """
 }
